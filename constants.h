a73f84c40421a41ee8c8ad09628b3998ee70cbcf
Terry Howald <terry@kesslercrane.com>
2018-11-15 21:46:31 -0500

    First commit

---
+++ constants.h
@@ -0,0 +1,14 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+// Define namespace to hold constants
+namespace constants
+{
+    constexpr double pi(3.14159);
+    constexpr double avogadro(6.0221413e23);
+    constexpr double my_gravity(9.2); // m/s^2 -- gravity is light on this planet
+    // ... other related constants
+    
+} // namespace constants
+
+#endif // CONSTANTS_H
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "constants.h"
+
+int main()
+{
+    std::cout << "Hello DRD3" << std::endl;
+    std::cout << "Pi = " << constants::pi << std::endl;
+
+    return 0;
+}
