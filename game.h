aa84dcc896da05a2850621d265b8d977fe800cf5
Terry Howald <terry@kesslercrane.com>
2018-11-20 23:10:07 -0500

    SDL2 path

--- game.h
+++ game.h
@@ -1,10 +1,10 @@
 #ifndef GAME_H
 #define GAME_H
 
-#include <SDL.h> 
-#include <SDL_image.h>
-#include <SDL_mixer.h>
-#include <SDL_ttf.h>
+#include <SDL2/SDL.h> 
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_mixer.h>
+#include <SDL2/SDL_ttf.h>
 
 class Game 
 {
