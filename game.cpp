db222dba37949626d59ba9d819d545f7162fbefe
Terry Howald <terry@kesslercrane.com>
2018-11-20 22:51:09 -0500

    Added in all SDL2 libraries

--- game.cpp
+++ game.cpp
@@ -24,6 +24,15 @@ Game::Game() : m_pWindow(nullptr), m_pRenderer(nullptr)
 
     // Initialize SDL2 Image library
     IMG_Init(IMG_INIT_PNG);
+
+    // Initialize SDL2 Mixer library
+    Mix_OpenAudio(48000, MIX_DEFAULT_FORMAT, 2, 4096);
+
+    // Initialize SDL2 TTF
+    if (TTF_Init() == -1)
+    {
+        std::cout << "Error : SDL_TTF" << std::endl;
+    }    
 }
 
 Game::~Game()
@@ -40,7 +49,13 @@ Game::~Game()
     {
         SDL_DestroyWindow(m_pWindow);
         m_pWindow = nullptr;
-    }    
+    }   
+
+    // Shutdown SDL2 TTF
+    TTF_Quit();    
+
+    // Shutdown SDL2 Mixer
+    Mix_CloseAudio();     
 
     // Shutdown SDL2 Image
     IMG_Quit();
